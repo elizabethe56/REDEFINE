a1186d6735e88a9e4522a146d51b2be9c8ab9e65
elizabethe56 <78177942+elizabethe56@users.noreply.github.com>
2023-10-05 11:22:55 -0400

    Establish strings file, init session states

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
 import streamlit as st
 import pandas as pd
+import json
 
+st.set_page_config(layout='wide')
+
+class App:
+
+    with open('strings.txt', 'r') as f:
+        __STRINGS = json.load(f)
+
+    def __init__(self):
+        # Preset session states; Used to preserve states on reruns/button pushes
+        if 'data' not in st.session_state:
+            st.session_state.data = ""
+        if 'has_data' not in st.session_state:
+            st.session_state.has_data = False
+        if 'raw_toggle' not in st.session_state:
+            st.session_state.raw_toggle = False
