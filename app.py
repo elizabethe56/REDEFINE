6943d23f21a602e97f26c8dd042e4e85354d4c65
elizabethe56 <78177942+elizabethe56@users.noreply.github.com>
2023-10-15 21:51:31 -0400

    param functionality,  reformatting

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ class App:
             st.session_state.has_data = False
         if 'raw_toggle' not in st.session_state:
             st.session_state.raw_toggle = False
+        if 'param_dict' not in st.session_state:
+            st.session_state.param_dict = {}
 
         return
     
@@ -58,10 +60,9 @@ class App:
     def window(self):
         # Title
         st.title(self.__STRINGS['Title'])
-
+        st.header(self.__STRINGS['Subtitle'])
         
-
-        col1, col2 = st.columns([1.5,2])
+        col1, col2 = st.columns([1,1])
 
         # col1: "Form"
         # - Target Column: target_input
@@ -72,7 +73,8 @@ class App:
         # - Run Button
         # - View Raw Data
 
-        # Data
+        col1.subheader(self.__STRINGS['Header1'])
+        ########## Data ##########
         data_file = col1.file_uploader(self.__STRINGS['Data_Entry'], 
                                        type = 'csv', 
                                        accept_multiple_files = False,
@@ -84,8 +86,7 @@ class App:
         has_data, data, data_err_msg = self.__load_data(data_file)
         st.session_state.has_data = has_data
         st.session_state.data = data
-            
-
+        
         if data_err_msg != "":
             data_err.error(data_err_msg)
 
@@ -93,18 +94,16 @@ class App:
         if st.session_state.has_data:
             cols.extend(list(data.columns))
 
-        # Target column and error message
+        ########## Target column ##########
         target_col = col1.selectbox(self.__STRINGS['Target_Entry'],
                                     options = cols.copy(),
                                     key = 'target_input')
-        target_err = col1.empty()
-        
-        if (st.session_state.has_data) and (target_col == self.__STRINGS['Default_Selectbox'][0]):
-            target_err.error(self.__STRINGS['Column_Error_Empty'])
-        
 
+        if (st.session_state.has_data) and (target_col == self.__STRINGS['Default_Selectbox'][0]):
+            col1.error(self.__STRINGS['Column_Error_Empty'])
+
+        ########## ID column and error message ##########
         cols.insert(1,self.__STRINGS['None_Selectbox_Option'])
-        # ID column and error message
         id_col = col1.selectbox(self.__STRINGS['ID_Entry'],
                                 options = cols.copy(),
                                 key = 'id_input')
@@ -112,19 +111,53 @@ class App:
 
         if (st.session_state.has_data) and (id_col == self.__STRINGS['Default_Selectbox'][0]):
             id_err.error(self.__STRINGS['Column_Error_Empty'])
-        if (st.session_state.has_data) and (id_col == target_col):
+        elif (st.session_state.has_data) and (id_col == target_col):
             id_err.error(self.__STRINGS['Column_Error_Dup'])
 
-        # Classifier Selector
+        # If data and columns are valid:
+
+
+        # Only show Classifier/Cluster options if data and columns are valid
+        
+        ########## Classifier Selector ##########
+        col1.markdown('-----')
+        col1.subheader(self.__STRINGS['Header2'])
+
         classifier = col1.selectbox(self.__STRINGS['Classifier_Entry'], 
                                     options = self.__STRINGS['Classifier_Options'], 
                                     key = 'classifier_input')
+        
+        subcol1, subcol2, subcol3, subcol4 = col1.columns(4)
+
+        # columns for parameter inputs
+        params = [subcol1.empty(), subcol2.empty(), subcol3.empty(), subcol4.empty()]
+        validate = subcol1.empty()
+
+
+        if classifier != self.__STRINGS['Default_Selectbox'][0]:
+            param_inputs = {}
+            # generate each parameter text input and collect their values
+            for i in range(len(params)):
+                param_name = self.__STRINGS["Classifier_Params"][classifier][i]
+                # Set default value to previous entry, otherwise empty
+                try:
+                    value = st.session_state.param_dict[classifier][param_name]
+                except:
+                    value = None
+                
+                temp = params[i].text_input(param_name, value)
+                param_inputs[param_name] = temp
+
+            st.session_state.param_dict[classifier] = param_inputs
+            
+            validate.button('Validate')
         
         # Cluster Selector
         cluster = col1.selectbox(self.__STRINGS['Cluster_Entry'], 
                                     options = self.__STRINGS['Cluster_Options'], 
                                     key = 'cluster_input')
 
+        # Raw Data Toggle
         raw_data = col1.toggle(self.__STRINGS['Show_Data_Toggle'],
                                key = 'raw_toggle',
                                disabled = not st.session_state.has_data)
